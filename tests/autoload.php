a35f1fd1ec417f9c2ab7a251ac949ec7cf59b7a6
Daniel Zahariev <daniel.zahariev@gmail.com>
2021-10-05 15:52:11 +0300

    Update test autoload

--- tests/autoload.php
+++ tests/autoload.php
@@ -7,8 +7,18 @@ if (empty($_ENV['SETTLE_MERCHANT_ID'])) {
     $dotenv->safeLoad();
 }
 
+$settle_public_key = $_ENV['SETTLE_PUBLIC_KEY'] ?? '';
+if ($settle_public_key && strpos($settle_public_key, 'PUBLIC') === false) {
+    $settle_public_key = base64_decode($settle_public_key);
+}
+
+$settle_private_key = $_ENV['SETTLE_PRIVATE_KEY'] ?? '';
+if ($settle_private_key && strpos($settle_private_key, 'PRIVATE') === false) {
+    $settle_private_key = base64_decode($settle_private_key);
+}
+
 define('SETTLE_MERCHANT_ID', $_ENV['SETTLE_MERCHANT_ID'] ?? '');
 define('SETTLE_USER_ID', $_ENV['SETTLE_USER_ID'] ?? '');
-define('SETTLE_PUBLIC_KEY', $_ENV['SETTLE_PUBLIC_KEY'] ?? '');
-define('SETTLE_PRIVATE_KEY', $_ENV['SETTLE_PRIVATE_KEY'] ?? '');
+define('SETTLE_PUBLIC_KEY', $settle_public_key);
+define('SETTLE_PRIVATE_KEY', $settle_private_key);
 define('SETTLE_IN_SANDBOX', ($_ENV['SETTLE_IN_SANDBOX'] ?? false) ? true : false);
