1cf7e746dcd8ccbb68b07243acfe0521292b4e13
Daniel Zahariev <daniel.zahariev@gmail.com>
2021-10-06 09:57:05 +0300

    Update test autoload

--- tests/autoload.php
+++ tests/autoload.php
@@ -2,24 +2,23 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (empty($_ENV['SETTLE_MERCHANT_ID'])) {
+if (empty($_ENV['SETTLE_MERCHANT_ID']) && empty($_SERVER['SETTLE_MERCHANT_ID'])) {
     $dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__ . '/../'));
     $dotenv->safeLoad();
 }
 
-$settle_public_key = $_ENV['SETTLE_PUBLIC_KEY'] ?? '';
+$settle_public_key = $_ENV['SETTLE_PUBLIC_KEY'] ?? $_SERVER['SETTLE_PUBLIC_KEY'] ?? '';
 if ($settle_public_key && strpos($settle_public_key, 'PUBLIC') === false) {
     $settle_public_key = base64_decode($settle_public_key);
 }
-print_r($_SERVER);
 
-$settle_private_key = $_ENV['SETTLE_PRIVATE_KEY'] ?? '';
+$settle_private_key = $_ENV['SETTLE_PRIVATE_KEY'] ?? $_SERVER['SETTLE_PRIVATE_KEY'] ?? '';
 if ($settle_private_key && strpos($settle_private_key, 'PRIVATE') === false) {
     $settle_private_key = base64_decode($settle_private_key);
 }
 
-define('SETTLE_MERCHANT_ID', $_ENV['SETTLE_MERCHANT_ID'] ?? '');
-define('SETTLE_USER_ID', $_ENV['SETTLE_USER_ID'] ?? '');
+define('SETTLE_MERCHANT_ID', $_ENV['SETTLE_MERCHANT_ID'] ?? $_SERVER['SETTLE_MERCHANT_ID'] ?? '');
+define('SETTLE_USER_ID', $_ENV['SETTLE_USER_ID'] ?? $_SERVER['SETTLE_USER_ID'] ?? '');
 define('SETTLE_PUBLIC_KEY', $settle_public_key);
 define('SETTLE_PRIVATE_KEY', $settle_private_key);
-define('SETTLE_IN_SANDBOX', ($_ENV['SETTLE_IN_SANDBOX'] ?? false) ? true : false);
+define('SETTLE_IN_SANDBOX', ($_ENV['SETTLE_IN_SANDBOX'] ?? $_SERVER['SETTLE_IN_SANDBOX'] ?? false) ? true : false);
